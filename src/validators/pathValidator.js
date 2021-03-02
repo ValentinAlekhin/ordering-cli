669560989465c5b21548b9163a13d479ff2b94b9
Valentin Alekhin <headbangernetwork@gmail.com>
2021-03-03 00:10:42 +0300

    Add options to path validator

--- src/validators/pathValidator.js
+++ src/validators/pathValidator.js
@@ -5,11 +5,21 @@ const chalk = require('chalk')
 
 const createError = message => new Error(chalk.red.bold(message))
 
-module.exports = path => {
-  if (!isValid(path)) {
+const def = {
+  validateString: true,
+  validateExists: true,
+}
+
+module.exports = (directory, options = def) => {
+  options = { ...def, ...options }
+  const { validateString, validateExists } = options
+
+  if (validateString && validateExists) return true
+
+  if (!isValid(directory) || !validateString) {
     throw createError('Должен быть указан путь')
   }
-  if (!fs.existsSync(path)) {
+  if (!fs.existsSync(directory) || validateExists) {
     throw createError('Указанной директории не существует')
   }
 
