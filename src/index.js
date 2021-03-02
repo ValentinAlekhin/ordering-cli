221708048cab51c7b7e16f1978b642d11fdbff98
Valentin Alekhin <headbangernetwork@gmail.com>
2021-03-02 04:35:03 +0300

    Add yargs

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ const fs = require('fs-extra')
 const path = require('path')
 const appRootPath = require('app-root-path').toString()
 
+const yargsOptions = require('./yargs/index')
+
 const CompareManager = require('./utils/CompareManager')
 const clusterMaster = require('./cluster/master')
 const workFunction = path.join(appRootPath, 'src', 'utils', 'comparePhotos.js')
@@ -10,14 +12,14 @@ const dataDir = path.join(appRootPath, 'data')
 const photos = fs.readdirSync(dataDir).map(el => path.join(dataDir, el))
 const compareManager = new CompareManager(photos)
 
-async function start() {
-  await clusterMaster(
-    compareManager.createWork.bind(compareManager),
-    compareManager.closeWork.bind(compareManager),
-    workFunction
-  )
+// async function start() {
+//   await clusterMaster(
+//     compareManager.createWork.bind(compareManager),
+//     compareManager.closeWork.bind(compareManager),
+//     workFunction
+//   )
 
-  console.log(compareManager.toRemove)
-}
+//   console.log(compareManager.toRemove)
+// }
 
-start()
+// start()
---
+++ src/yargs/index.js
@@ -0,0 +1,15 @@
+const yargs = require('yargs')
+
+module.exports = yargs
+  .scriptName('ordering-cli')
+  .usage('Usage: $0 [args]')
+  .option('root', {
+    alias: 'root',
+    describe: 'Указать рабочую директорию',
+    type: 'string',
+  })
+  .alias('r', 'root')
+  .describe('r', 'Указать рабочую директорию')
+  .alias('h', 'help')
+  .alias('v', 'version')
+  .help('h').argv
