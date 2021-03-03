9b827dfcf6c598f270323dd82b83360c38f7dbcd
Valentin Alekhin <headbangernetwork@gmail.com>
2021-03-03 18:19:47 +0300

    Add app path validation

--- src/validators/pathValidator.js
+++ src/validators/pathValidator.js
@@ -1,5 +1,6 @@
 const fs = require('fs-extra')
 
+const appRootPath = require('app-root-path').path
 const isValid = require('is-valid-path')
 const chalk = require('chalk')
 
@@ -8,19 +9,23 @@ const createError = message => new Error(chalk.red.bold(message))
 const def = {
   validateString: true,
   validateExists: true,
+  validateAppPath: false,
 }
 
 module.exports = (directory, options = def) => {
   options = { ...def, ...options }
-  const { validateString, validateExists } = options
+  const { validateString, validateExists, validateAppPath } = options
 
-  if (validateString && validateExists) return true
-
-  if (!isValid(directory) || !validateString) {
+  if (!isValid(directory) && !validateString) {
     throw createError('Должен быть указан путь')
   }
-  if (!fs.existsSync(directory) || validateExists) {
+  if (!fs.existsSync(directory) && !validateExists) {
     throw createError('Указанной директории не существует')
+  }
+  if (directory === appRootPath && validateAppPath) {
+    throw createError(
+      'Комманда не может быть выполненна в директории приложения'
+    )
   }
 
   return true
