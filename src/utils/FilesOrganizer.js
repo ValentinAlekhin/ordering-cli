c4abc6f137b475220d5048d0b1e5a4586134660d
Valentin Alekhin <headbangernetwork@gmail.com>
2021-03-04 17:39:33 +0300

    Reafactor FileOrganizer

--- src/utils/FilesOrganizer.js
+++ src/utils/FilesOrganizer.js
@@ -1,101 +1,104 @@
+//TODO: Добавить удаление старых директорий
+//TODO: Добавить создание только необходимых директорий
+
 const fs = require('fs-extra')
 const path = require('path')
 
 const FileType = require('file-type')
-
-const def = {
-  files: {
-    image: {
-      dirName: 'image',
-    },
-    video: {
-      dirName: 'video',
-    },
-    audio: {
-      dirName: 'audio',
-    },
-    other: { dirName: 'other' },
-  },
-  database: {
-    dir: '',
-    name: 'database.json',
-  },
-}
+const { realpathSync } = require('fs')
 
 class FilesOrganizer {
-  constructor(directory, options = def) {
-    this.directory = directory
-    this.options = {
-      files: {
-        image: { ...def.files.image, ...options.files.image },
-        video: { ...def.files.video, ...options.files.video },
-        other: { ...def.files.other, ...options.files.other },
-      },
-      database: { ...def.database, ...options.database },
-    }
-
-    this.createFilesPath()
-    this.createDirectories()
+  constructor(directory) {
+    this.directory = path.normalize(directory)
+    this.files = {}
+    this.currentFile = {}
   }
 
   async start() {
     await this.recursiveSearch(this.directory)
 
-    return this.options.files
+    return this.files
   }
 
-  createFilesPath() {
-    Object.entries(this.options.files).forEach(file => {
-      const [key, { dirName }] = file
-      this.options.files[key].path = path.join(this.directory, dirName)
-    })
+  isFileTypeDirExists() {
+    return this.files.hasOwnProperty(this.currentFile.type)
   }
 
-  createDirectories() {
-    const newDirectories = Object.values(this.options.files).map(
-      fileType => fileType.path
-    )
+  createFilePath() {
+    const newFilePath = path.join(this.directory, this.currentFile.type)
 
-    for (const newDirPath of newDirectories) {
-      const isDirExists = fs.existsSync(newDirPath)
+    this.files[type].path = newFilePath
 
-      if (isDirExists) continue
+    return newFilePath
+  }
 
-      fs.mkdirSync(newDirPath)
+  createFileDirectory() {
+    const { type } = this.currentFile
+
+    if (this.isFileTypeDirExists()) return this.files[type].path
+
+    const newFilePath = this.createFilePath()
+
+    fs.mkdirSync(newFilePath)
+
+    return newFilePath
+  }
+
+  createFileName() {
+    if (this.isFileHasRightExt()) return
+
+    const { rightExt, base } = this.currentFile
+
+    this.name = `${base}.${rightExt}`
+  }
+
+  isFileHasRightExt() {
+    const { rightExt, currentExt } = this.currentFile
+
+    return rightExt === currentExt
+  }
+
+  isFileOnRightDir() {
+    return this.files[this.currentFile.type].path === this.currentFile.dir
+  }
+
+  async setCurrentFile(file) {
+    try {
+      const { ext: currentExt, base, name, dir } = path.parse(file)
+      const { mime, ext: rightExt } = await FileType.fromFile(file)
+      const type = mime.split('/')[0]
+
+      this.currentFile = { currentExt, base, name, dir, rightExt, type, file }
+    } catch (e) {
+      throw new Error(e)
     }
+  }
+
+  renameFile() {
+    const { file, name, dir } = this.currentFile
+
+    const newPath = path.join(dir, name)
+
+    fs.renameSync(file, newPath)
   }
 
   async fileHandler(file) {
     try {
-      const { ext: oldExt, base, name, dir } = path.parse(file)
+      await this.setCurrentFile()
+      this.createFileName()
 
-      const { mime, ext } = await FileType.fromFile(file)
-      const fileType = mime.split('/')[0]
-
-      const baseName = `${name}.${ext}`
-      let targetDir
-
-      if (this.options.files.hasOwnProperty(fileType)) {
-        targetDir = this.options.files[fileType].path
-      } else {
-        targetDir = this.options.files.other.path
+      if (this.isFileOnRightDir()) {
+        if (!this.isFileHasRightExt()) {
+          this.renameFile()
+          return
+        }
       }
 
-      if (dir === targetDir) {
-        if (oldExt.toLocaleLowerCase() === ext.toLocaleLowerCase()) return
-        await fs.rename(file, path.join(targetDir, baseName))
-        return
-      }
+      const { name } = this.currentFile
 
-      let newPath = path.join(targetDir, base)
+      const newFilePath = path.join(this.createFileDirectory(), name)
 
-      if (fs.existsSync(newPath)) {
-        newPath = path.join(targetDir, `${name}.copy${ext}`)
-      } else {
-        newPath = path.join(targetDir, baseName)
-      }
-
-      await fs.copyFile(file, newPath)
+      await fs.copyFile(file, newFilePath)
       await fs.remove(file)
     } catch (e) {
       throw new Error(e)
