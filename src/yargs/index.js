a6b4c7d7c8ab4e38a8f8494c69ed7819d4a55078
Valentin Alekhin <headbangernetwork@gmail.com>
2021-03-06 19:55:17 +0300

    Add commpents for navigation

--- src/yargs/index.js
+++ src/yargs/index.js
@@ -7,20 +7,26 @@ const pathValidator = require('../validators/path/path')
 module.exports = yargs
   .scriptName('ordering-cli')
   .usage('Usage: $0 [args]')
+
+  // Root option
   .option('root', {
     alias: 'r',
     describe: 'Указать рабочую директорию',
     type: 'string',
-    default: process.cwd() === appRootPath ? null : process.cwd(),
+    default: process.cwd().includes(appRootPath()) ? null : process.cwd(),
   })
   .check(({ root }) =>
     root ? pathValidator(root, { validateAppPath: true }) : true
   )
-  .option('remove-copy', {
+
+  //Remove same option
+  .option('remove-same', {
     describe: 'Удалить одинаковые фото',
     type: 'boolean',
     default: false,
   })
+
+  //Other options
   .alias('v', 'version')
   .help('h')
   .alias('h', 'help').argv
